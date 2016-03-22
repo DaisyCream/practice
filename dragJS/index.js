de8faa96f56d4c9f7e6ed9574bfa3734184bcdec
DaisyCream@qq.com <DaisyCream@qq.com>
2016-03-22 15:42:19 +0800

    add the mouse

--- dragJS/index.js
+++ dragJS/index.js
@@ -10,14 +10,20 @@ console.log(drager);
 function  mouseDrag(target){
     var isDrag = 0;
     var pos = {};
+    var mouse = {};
     //target.style.webkitTransition = 'top 0.2s, left 0.2s';
     target.onmousedown = function(e){
-        console.log("onmousedown");
+        //console.log("onmousedown");
         e = e || window.event;
+        mouse.x = e.clientX-parseInt(target.style.left);
+        mouse.y = e.clientY-parseInt(target.style.top);
+        //console.log(mouse.x,mouse.y);
+        //console.log(e.clientX,e.clientY);
+        //console.log(target.style.left,target.style.top);
         isDrag = 1;
     };
     target.onmouseup = function(e){
-        console.log("onmouseup");
+        //console.log("onmouseup");
         if(isDrag==1){
             POSITION.setPosition(pos.x,pos.y,target);
             isDrag = 0;
@@ -28,10 +34,11 @@ function  mouseDrag(target){
         //console.log(e.clientX, e.clientY);
         if(isDrag == 1) {
             //console.log("onmousemove");
-            target.style.left = e.clientX + 'px';
-            target.style.top = e.clientY + 'px';
-            pos.x = e.clientX;
-            pos.y = e.clientY;
+            //console.log(mouse.x,mouse.y,e.clientX, e.clientY);
+            target.style.left = e.clientX - mouse.x  + 'px';
+            target.style.top = e.clientY - mouse.y + 'px';
+            pos.x = target.style.left;
+            pos.y = target.style.top;
             //console.log(e.clientX, e.clientY);
         }
     };
@@ -40,8 +47,8 @@ function  mouseDrag(target){
 
 
 POSITION.setPosition = function(x,y,target){
-    target.style.left = x + 'px';
-    target.style.top = y + 'px';
+    target.style.left = x;
+    target.style.top = y;
 };
 
 mouseDrag(drager);
